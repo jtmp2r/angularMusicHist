1bfb7f357f54c14293280b3ad5bf04f9749df687
jtmp2r <jtmp2r@gmail.com>
2015-11-14 10:36:10 -0600

    using factory provider

--- angularMusicHist/main.js
+++ angularMusicHist/main.js
@@ -1,19 +1,101 @@
-var app = angular.module("MusicHis", [])
+var app = angular.module("musicHistory", ['ngRoute'])
 
-app.controller("MusicHis", function($scope) {
-	$scope.title = "THIS IS MUSIC!!!";
-  
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	  .when('/songs/list', {
+	  	templateUrl: "partials/song-list.html",
+	  	controller: 'SongListCtrl'
+	  })
+	  .when('/songs/new', {
+	  	templateUrl: "partials/song-list.html",
+	  	controller: 'addSongCtrl'
 
-  $scope.songS =
-	  	getList: function(getData) {
-	    	$.ajax({
-		  		url: "songs.json"
-		  	}).done(function(songList) {
-	    		 return getData(songList);
-	 			})
-	 	  }	
-    
-  	
- 
+	  })
+}]);
 
-})
+app.factory('song_service', function($http, $q) {
+	var songList = [];
+	
+  function init() {
+    return $q(function(resolve, reject) {
+    $http
+      .get('./data/songs.json')
+      .success(
+        function(objectFromJSONFile) {
+          songList = objectFromJSONFile.songs;
+          resolve(songList)
+        },function(error) {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  init();
+
+  function getSongs(){
+    return songList;
+  }
+
+  function getSingleSong(id) {
+    return songList.filter(function(song){
+      return song.id === id;
+    })[0];
+  }
+
+  function addSong(songObj) {
+    songList.push(songObj);
+    return songList;
+  }
+
+  return {
+    getSongs: getSongs,
+    getSingleSong: getSingleSong,
+    addSong: addSong
+  };
+}); //end factory
+
+
+app.controller("SongListCtrl",
+  [
+    "$scope",
+    "song_service",
+    function($scope, song_service ) {
+      // get initial list of songs on page load
+      $scope.songs_list = song_service.getSongs();
+    }
+  ]
+);
+
+app.controller("AddSongCtrl",
+  [
+    "$scope",
+    "song_service",
+    function($scope, song_service ) {
+      $scope.newSong = { title: "", album: "", year: "", artist: "" };
+
+      $scope.addSong = function() {
+        song_service.addSong({
+          artist: $scope.newSong.artist,
+          title: $scope.newSong.title,
+          album: $scope.newSong.album,
+          year: $scope.newSong.year
+        });
+      };
+    }
+  ]
+);
+
+
+// app.controller('SongListCtrl', function($scope) {
+//   $.ajax('songs.json')
+//   .done(function(ajaxSongs) {
+//   	console.log("ajax")
+//   	$scope.songs = ajaxSongs.songs;
+//   	console.log(ajaxSongs.songs);
+//   	$scope.$apply();
+//   })
+//   .fail(function(error) {
+//   	console.log("error", error)
+//   });
+// });
